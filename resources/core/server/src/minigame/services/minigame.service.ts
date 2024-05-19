ecdeaf631fe4cd5a4c30744398136b5c270e6d7b
Slowmoney <frolenkov58@gmail.com>
2024-05-19 22:35:44 +0300

    rm: tests

--- resources/core/server/src/minigame/services/minigame.service.ts
+++ resources/core/server/src/minigame/services/minigame.service.ts
@@ -1,7 +1,5 @@
 import { Inject, Injectable } from "@altv-mango/core";
 import * as alt from "@altv/server";
-import assert from "assert";
-import { randomInt } from "crypto";
 import { HandState } from "@shared";
 import { NotifyService } from "../../notify/services/notify.service";
 import { PlayerService } from "../../player/services/player.service";
@@ -11,130 +9,7 @@ import { MINIGAME_REPOSITORY } from "../providers/minigame-repository.provider";
 
 @Injectable()
 export class MinigameService{
-
-    constructor(@Inject(PlayerService) private playerService: PlayerService, @Inject(MINIGAME_REPOSITORY) private minigameRepository: MinigameRepository, @Inject(NotifyService) private notifyService: NotifyService) {
-        this.test1()
-        this.test2()
-        this.test3()
-        this.test4()
-        this.test5()
-        this.test6([1, 2, 3, 4, 5])
-    }
-
-    test1() {
-        const minigame = this.minigameRepository.create([1, 2])
-        this.setHandState(1, HandState.Paper)
-        this.setHandState(2, HandState.Paper)
-        const result = minigame.getState()
-        console.log(result)
-        assert(result.drawPlayers.includes(1) && result.drawPlayers.includes(2) && result.drawPlayers.length === 2 && result.winnerPlayers.length === 0 && result.loosePlayers.length === 0, "test 2 draw battles")
-        this.minigameRepository.clear(minigame)
-    }
-
-    test2() {
-        const minigame = this.minigameRepository.create([1, 2])
-        this.setHandState(1, HandState.Paper)
-        this.setHandState(2, HandState.Rock)
-
-        const result = minigame.getState()
-        console.log(result)
-        assert(result.winnerPlayers.length === 1, 'too more winners')
-        assert(result.loosePlayers.length === 1, 'too more loosers')
-        assert(result.winnerPlayers.includes(1), 'no winner')
-        assert(result.loosePlayers.includes(2), 'no looser')
-        assert(result.drawPlayers.length === 0, 'no valid draw state')
-        this.minigameRepository.clear(minigame)
-    }
-
-    test3() {
-        const minigame = this.minigameRepository.create([1, 2, 3])
-        this.setHandState(1, HandState.Paper)
-        this.setHandState(2, HandState.Rock)
-        this.setHandState(3, HandState.Rock)
-
-        const result = minigame.getState()
-        console.log(result)
-        assert(result.winnerPlayers.length === 1, 'too more winners')
-        assert(result.loosePlayers.length === 2, 'too more loosePlayers')
-        assert(result.drawPlayers.length === 2, 'too more drawPlayers')
-        assert(result.winnerPlayers.includes(1), 'no winner')
-        assert(result.loosePlayers.includes(2), 'no looser 2')
-        assert(result.loosePlayers.includes(3), 'no looser 3')
-        assert(result.drawPlayers.includes(2), 'no draw 2')
-        assert(result.drawPlayers.includes(3), 'no draw 3')
-        this.minigameRepository.clear(minigame)
-    }
-
-    test4() {
-        const minigame = this.minigameRepository.create([1, 2, 3])
-        this.setHandState(1, HandState.Paper)
-        this.setHandState(2, HandState.Rock)
-        this.setHandState(3, HandState.Scissors)
-
-        const result = minigame.getState()
-        console.log(result)
-        assert(result.winnerPlayers.length === 0, 'too more winners')
-        assert(result.loosePlayers.length === 0, 'too more loosePlayers')
-        assert(result.drawPlayers.length === 3, 'too more drawPlayers')
-        assert(result.drawPlayers.includes(1), 'no draw 1')
-        assert(result.drawPlayers.includes(2), 'no draw 2')
-        assert(result.drawPlayers.includes(3), 'no draw 3')
-        this.minigameRepository.clear(minigame)
-    }
-
-    test5() {
-        const minigame = this.minigameRepository.create([1, 2, 3, 4, 5])
-        this.setHandState(1, HandState.None)
-        this.setHandState(2, HandState.None)
-        this.setHandState(3, HandState.Scissors)
-
-        const result = minigame.getState()
-        console.log(result)
-        assert(result.winnerPlayers.length === 0, 'too more winners')
-        assert(result.loosePlayers.length === 0, 'too more loosePlayers')
-        assert(result.drawPlayers.length === 5, 'too more drawPlayers')
-        assert(result.drawPlayers.includes(1), 'no draw 1')
-        assert(result.drawPlayers.includes(2), 'no draw 2')
-        assert(result.drawPlayers.includes(3), 'no draw 3')
-        assert(result.drawPlayers.includes(4), 'no draw 4')
-        assert(result.drawPlayers.includes(5), 'no draw 5')
-        this.minigameRepository.clear(minigame)
-    }
-
-    test6(players: number[]) {
-        console.log("run test 6", players)
-        const minigame = this.minigameRepository.create(players)
-        for (const player of players) {
-            const value = randomInt(0, 3)
-            if (value === 0) this.setHandState(player, HandState.Paper)
-            else if (value === 1) this.setHandState(player, HandState.Rock)
-            else this.setHandState(player, HandState.Scissors)
-        }
-        const result = minigame.getState()
-        this.minigameRepository.clear(minigame)
-
-        if (result.drawPlayers.length) {
-            //console.log("draw", players, result)
-
-            setImmediate(() => this.test6(result.drawPlayers))
-        } else {
-            //console.log(result)
-        }
-        if (result.winnerPlayers.length) {
-            if (result.winnerPlayers.length > 1) {
-                setImmediate(() => this.test6(result.winnerPlayers))
-
-            } else {
-                console.log("winner", players, result)
-            }
-        } else {
-            //console.log(result)
-        }
-
-        /* this.test6(result.loosePlayers)
-
-        this.test6(result.winnerPlayers) */
-    }
+    constructor(@Inject(PlayerService) private playerService: PlayerService, @Inject(MINIGAME_REPOSITORY) private minigameRepository: MinigameRepository, @Inject(NotifyService) private notifyService: NotifyService) {}
 
     public startGameInRadius(player: alt.Player, radius = 10) {
         const minigame = this.minigameRepository.findByPlayerId(player.id)
